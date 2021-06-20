8940776c9b908bb96be05ab06e70d3c3019dfd31
Marzouq <>
2021-06-20 11:05:57 +0000

    withfunction

--- app.js
+++ app.js
@@ -14,21 +14,30 @@ while (niceflowers != "grape" && niceflowers != "olives" && niceflowers != "coco
 
 
 if (niceflowers == "grape") {
-   document.write("<p>" + "nice! we have a plenty of tasty grapes ")
-    }
+  document.write("<p>" + "nice! we have a plenty of tasty grapes ")
+}
 
 else if (niceflowers == "olives") {
-   document.write("<p>" + "nice! we have a plenty of original olives")
-  }
+  document.write("<p>" + "nice! we have a plenty of original olives")
+}
 
 else if (niceflowers == "coconut") {
-   document.write("<p>" + "nice! we have a plenty of original coconut") 
-   }
+  document.write("<p>" + "nice! we have a plenty of original coconut")
+}
 else {
   console.log("sorry, we didn't have your order")
 }
 
-var numofflowers=prompt("how many times you would like to see flowers?")
+function rating() {
+  var numofstars = prompt("how many stras you would like to rate my website?")
+  return numofstars;
+}
+var num = rating();
+console.log(num)
 
-for (var i=0; i < numofflowers; i++)
-{document.write("<img src='https://www.alfakher50g.com/wp-content/uploads/2020/09/purple-grape-600x600.png'>")}
+for (var i = 0; i < num; i++) {
+  document.write("<img src='https://static4.depositphotos.com/1001003/335/i/600/depositphotos_3350007-stock-photo-3d-star.jpg' />")
+  
+}
+
+
